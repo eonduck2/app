66b389f2b91083065441efdf01efaea7eae26f74
이종수 <whdtnxd@gmail.com>
2024-08-20 16:03:26 +0900

    ㅋㅋ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div
-      className="w-screen h-screen bg-white overflow-hidden flex justify-center items-center gap-20"
+      className="w-svw h-svh bg-white overflow-hidden flex justify-center items-center gap-20"
       style={{
         backgroundImage:
           'url("https://i.pinimg.com/564x/e0/97/81/e097814065839fdd1877617ce43e1696.jpg")',
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center", // 이미지가 div의 중앙에 위치하도록 설정
+        backgroundSize: "cover", // 이미지가 전체 div를 덮도록 설정
       }}
     >
-      <div className="w-28 h-24 bg-red-400" />
-      <div className="w-28 h-24 bg-red-400" />
+      <h1 className="text-7xl text-black font-semibold">
+        서브웨이를 주문했어요
+      </h1>
     </div>
   );
 }
