4af548ffddd4f23c65b9f608210baa4edbfa1146
이종수 <whdtnxd@gmail.com>
2024-08-20 17:00:51 +0900

    ㅋㅋ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const QRCode = dynamic(() => import("qrcode.react"), { ssr: false });
+
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <div
-      className="w-svw h-svh bg-white overflow-hidden flex justify-center items-center gap-20"
+      className="w-svw h-svh bg-white overflow-hidden flex justify-center flex-col items-center gap-20"
       style={{
         backgroundImage:
           'url("https://i.pinimg.com/564x/e0/97/81/e097814065839fdd1877617ce43e1696.jpg")',
         backgroundRepeat: "no-repeat",
-        backgroundPosition: "center", // 이미지가 div의 중앙에 위치하도록 설정
-        backgroundSize: "cover", // 이미지가 전체 div를 덮도록 설정
+        backgroundPosition: "center",
+        backgroundSize: "cover",
       }}
     >
       <h1 className="text-8xl text-black font-semibold">AWS를 마스터 해봐요</h1>
+      {isClient && (
+        <QRCode
+          value="https://release.d20qj9z66btjk0.amplifyapp.com"
+          size={130}
+        />
+      )}
     </div>
   );
 }
